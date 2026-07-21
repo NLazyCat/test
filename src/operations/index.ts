1018d9dd06ae135c2f61b6731841176694f0f28d
A_YZYZ <94771891+NLazyCat@users.noreply.github.com>
2026-07-21 12:40:58 +0800

    Delete src directory

--- src/operations/index.ts
+++
@@ -1,5 +0,0 @@
-export { add, addDescriptor } from "./add";
-export { subtract, subtractDescriptor } from "./subtract";
-export { multiply, multiplyDescriptor } from "./multiply";
-export { divide, divideDescriptor } from "./divide";
-export { modulo, moduloDescriptor } from "./modulo";
